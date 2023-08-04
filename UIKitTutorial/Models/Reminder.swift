f606f164b798559530ca146bebfad8ac331caad7
naokiwakata <rockwakata15@gmail.com>
2023-08-04 17:50:39 +0900

    first commit

---
+++ UIKitTutorial/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  UIKitTutorial
+//
+//  Created by naoki-wakata on 2023/08/04.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
