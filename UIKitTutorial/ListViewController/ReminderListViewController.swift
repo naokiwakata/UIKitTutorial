a970b3cbea885eba10a76ea43c303ab6ec093dec
naokiwakata <rockwakata15@gmail.com>
2023-08-07 18:47:16 +0900

    UIKit essentials

---
+++ UIKitTutorial/ListViewController/ReminderListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ReminderListViewController.swift
+//  UIKitTutorial
+//
+//  Created by naoki-wakata on 2023/08/07.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+    var dataSource: DataSource!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        dataSource.apply(snapshot)
+
+        collectionView.dataSource = dataSource
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
